706dbd0cb6e9aa2e2aa3d9ed65264ccfef7742de
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2023-04-07 11:44:33 +0530

    update

--- vars/excelLoader.groovy
+++ vars/excelLoader.groovy
@@ -1,7 +1,11 @@
+#!/usr/bin/env groovy
+
 def excelLoader(body){
     echo "Start Deploy"
     new Deployer(script:this).run()
     echo "Deployed"
 
+    currentBuild.result = 'SUCCESS' //FAILURE to fail
+    
     return this
 }
