bb935fc3ea2f810eccbf570d92f4a4a7a2112075
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2023-04-10 11:42:36 +0530

    update

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -8,17 +8,18 @@ class Deployer{
     Script script
 
     def run(){
-        Workbook workbook = Workbook.getWorkbook(new File("GPF_KLI_Dummy.xlsx"))
-        Sheet sheet1 = workbook.getSheet("DataDriven")
-        def rows = sheet1.getRows()
-        def cols = sheet1.getColumns()
-        log.info "Row Count =" + rows 
-        log.info "Column Count =" + cols 
-        for(i=1;i<rows;i++) { 
-            for(j=0;j<cols;j++) { 
-            Cell cell = sheet1.getCell(j,i) 
-            log.info cell.getContents() 
-            } 
-        }
+        script.echo("$WORKSPACE")
+        // Workbook workbook = Workbook.getWorkbook(new File("GPF_KLI_Dummy.xlsx"))
+        // Sheet sheet1 = workbook.getSheet("DataDriven")
+        // def rows = sheet1.getRows()
+        // def cols = sheet1.getColumns()
+        // log.info "Row Count =" + rows 
+        // log.info "Column Count =" + cols 
+        // for(i=1;i<rows;i++) { 
+        //     for(j=0;j<cols;j++) { 
+        //     Cell cell = sheet1.getCell(j,i) 
+        //     log.info cell.getContents() 
+        //     } 
+        // }
     }
 }
