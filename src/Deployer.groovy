0f4130ae6f381c89c85e6a165a9252beda958275
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2023-04-10 11:41:25 +0530

    update

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -1,33 +1,24 @@
-@Grab(group = 'org.codehaus.groovy.modules.scriptom', module='scriptom', version = '1.6.0')
 // @Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
 // import org.apache.commons.lang3.StringUtils
-import org.codehaus.groovy.scriptom.ActiveXObject
+@Grab(group = 'net.sourceforge.jexcelapi', module = 'jxl', version = '2.6.12')
+import jxl.*
 
 class Deployer{
     // int tries = 0
     Script script
 
     def run(){
-        def excelObj = new ActiveXObject('Excel.Application')
-        
-        def workBook = excelObj.Workbooks.Open("GPF_KLI_Dummy.xlsx")
-
-        //To get a sheet by index
-        def sheetByIndex = workBook.Sheets.Item[1]
-        
-        //To get the active sheet in the workbook
-        def sheetActive = workBook.ActiveSheet
-        
-        //To get the sheet by name
-        def sheetByName = workBook.Sheets('Sheet1')
-
-        def cellValue = sheet.Cells(2,1).Value
-
-        // while (tries < 10) {
-        //     Thread.sleep(1000)
-        //     tries++
-        //     script.echo("tries is numeric: " + StringUtils.isAlphanumeric("" + tries))
-        // }
-
+        Workbook workbook = Workbook.getWorkbook(new File("GPF_KLI_Dummy.xlsx"))
+        Sheet sheet1 = workbook.getSheet("DataDriven")
+        def rows = sheet1.getRows()
+        def cols = sheet1.getColumns()
+        log.info "Row Count =" + rows 
+        log.info "Column Count =" + cols 
+        for(i=1;i<rows;i++) { 
+            for(j=0;j<cols;j++) { 
+            Cell cell = sheet1.getCell(j,i) 
+            log.info cell.getContents() 
+            } 
+        }
     }
 }
