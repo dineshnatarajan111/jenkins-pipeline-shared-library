101fa6a2decf1ceaf13dc42ef811ab1c973b568f
dineshnatarajan5 <dineshnatarajan5@gmail.com>
2023-04-11 18:07:40 +0530

    update

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -21,8 +21,6 @@ class Deployer{
             Cell cell_KLI_branch = sheet1.getCell(3,i)
             Cell cell_GPEF_branch = sheet1.getCell(5,i)
 
-            script.echo(cell_KLI_branch.getContents() + " KLI")
-            script.echo(cell_GPEF_branch.getContents() + " GPERF")
             if(cell_KLI_branch.getContents() == cell_GPEF_branch.getContents()){
                 script.echo(cell_KLI_branch.getContents() + " KLI H")
                 script.echo(cell_GPEF_branch.getContents() + " GPERF H")
