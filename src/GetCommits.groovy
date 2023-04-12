e6a80cb703a9c799a564fa633571bb5afc70b05e
dineshnatarajan5 <dineshnatarajan5@gmail.com>
2023-04-12 09:06:15 +0530

    added git command

--- src/GetCommits.groovy
+++ src/GetCommits.groovy
@@ -2,7 +2,11 @@ class GetCommits{
     Script script
 
     def commits(body){
-        script.echo("$body.KLI_img")
-        script.echo("$body.GPEF_img")
+        // script.echo("$body.KLI_img")
+        // script.echo("$body.GPEF_img")
+
+        script{
+            git shortlog body.KLI_img body.GPEF_img
+        }
     }
 }
