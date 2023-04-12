39c9e52e3c5fef9acbc2a65a8690406d2cf3c239
dineshnatarajan5 <dineshnatarajan5@gmail.com>
2023-04-12 15:18:42 +0530

    update

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,9 @@
 def call(Map stageParams) {
-    echo "$stageParams.branches"
-    echo "$stageParams.url"
+    
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branches ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        userRemoteConfigs: [[ url: stageParams.url, credentialsId: stageParams.credentialsId ]]
     ])
+
   }
