1c0afb0f941cb6ee05b690be4615f85cee5a468b
dineshnatarajan5 <dineshnatarajan5@gmail.com>
2023-04-12 10:02:12 +0530

    update

--- src/GetCommits.groovy
+++ src/GetCommits.groovy
@@ -2,8 +2,8 @@ class GetCommits{
     Script script
 
     def commits(body){
-        // script.echo("$body.KLI_img")
-        // script.echo("$body.GPEF_img")
+        script.echo("$body.KLI_img")
+        script.echo("$body.GPEF_img")
 
         script.bat "git shortlog ${body.KLI_img} ${body.GPEF_img}"
     }
