25b963134adf00844a4f12f8acfd7e226ee6c608
dineshnatarajan5 <dineshnatarajan5@gmail.com>
2023-04-12 08:59:11 +0530

    update

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -27,7 +27,7 @@ class Deployer{
             if(cell_KLI_branch.getContents() == cell_GPEF_branch.getContents()){
                 script.echo(cell_KLI_branch.getContents() + " KLI H")
                 script.echo(cell_GPEF_branch.getContents() + " GPERF H")
-                getcommits.commits(KLI_img: cell_KLI_img.getContents() , GPEF_img: cell_GPEF_img.getContents())
+                getcommit.commits(KLI_img: cell_KLI_img.getContents() , GPEF_img: cell_GPEF_img.getContents())
                 // new GetCommits(script:this).commits([KLI_img: cell_KLI_img.getContents() , GPEF_img: cell_GPEF_img.getContents() ])
             }
             // for(int j=0;j<cols;j++) { 
