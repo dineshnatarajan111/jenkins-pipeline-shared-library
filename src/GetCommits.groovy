7c511652be81e58659bdb90d15a72354c64fb893
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2023-04-13 22:00:57 +0530

    update

--- src/GetCommits.groovy
+++ src/GetCommits.groovy
@@ -1,16 +1,16 @@
 class GetCommits implements Serializable{
     Script script
 
-    def commits(body){
-        def KLI = body.KLI_img
-        def GPEF = body.GPEF_img
+    // def commits(body){
+    //     def KLI = body.KLI_img
+    //     def GPEF = body.GPEF_img
 
-        script.echo("$KLI")
-        script.echo("$GPEF")
+    //     script.echo("$KLI")
+    //     script.echo("$GPEF")
 
-        script.echo("$script")
+    //     script.echo("$script")
 
-    }
+    // }
 
     def bat_cmd(args){
         script.bat "git shortlog ${args.KLI_img} ${args.GPEF_img}"
