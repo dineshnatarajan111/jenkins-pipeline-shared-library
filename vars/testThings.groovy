2c83ea412d6814d36e084a133a35e46abf868535
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2023-04-13 21:59:11 +0530

    update

--- vars/testThings.groovy
+++ vars/testThings.groovy
@@ -1,6 +1,6 @@
 def call(body){
     echo "Start Deploy"
-    new GetCommits(script:this).run(body)
+    new GetCommits(script:this).bat_cmd(body)
     echo "Deployed"
 
     currentBuild.result = 'SUCCESS' //FAILURE to fail
